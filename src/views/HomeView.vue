99772d316aee071ddd5a9f074ef881e7db29750e
binghan60 <binghan60@gmail.com>
2024-10-28 12:19:52 +0800

    首頁切版調整

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <script>
-import NavNarComponent from '@/components/NavNarComponent.vue'
+import { RouterView } from 'vue-router'
 
 export default {
-  components: { NavNarComponent },
+  components: { RouterView },
 }
 </script>
 
 <template>
-  <NavNarComponent></NavNarComponent>
   <div class="wraper">
     <div class="container">
       <div class="logo">
@@ -18,14 +17,19 @@ export default {
         </div>
       </div>
     </div>
-
+    <div class="spacer"></div>
+    <div class="main container">
+      <h3>Since 2022</h3>
+      <button type="button">立即購買</button>
+    </div>
     <div class="spacer"></div>
   </div>
+  <RouterView></RouterView>
 </template>
 <style scoped>
 .wraper {
   width: 100%;
-  height: calc(100vh - 60px);
+  height: calc(100vh - var(--navbar-height));
   background-image: url('/img/07f51b1cea86669fc9578201c74d3053.webp');
   background-size: cover;
   background-position: center;
@@ -33,12 +37,13 @@ export default {
   color: var(--text);
   display: flex;
   flex-direction: column;
+  flex: 1;
 }
 .wraper .box {
   width: 150px;
   text-align: center;
   font-weight: 700;
-  padding-top: 50px;
+  padding-top: var(--navbar-height);
   margin: 0 0 auto 0;
 }
 .box img {
@@ -55,13 +60,33 @@ export default {
   text-align: left;
 }
 
+.wraper h3 {
+  font-size: 56px;
+  font-weight: 900;
+  margin-bottom: 25px;
+}
+
+.main {
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 36px;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.75);
+}
+.main h3 {
+  font-size: 108px;
+}
 .main button {
-  font-size: 30px;
+  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  line-height: 56px;
+  font-size: 24px;
   font-weight: 700;
-  padding: 5px 50px;
-  background-color: #ffffff00;
-  border: 3px solid var(--secondary-color);
-  color: var(--secondary-color);
+  padding: 0px 60px;
+  background-color: #211b1b00;
+  border: 3px solid var(--white);
+  color: var(--white);
   cursor: pointer;
   white-space: nowrap;
 }
@@ -69,9 +94,34 @@ export default {
   flex: 1;
 }
 
+/* 小螢幕 */
 @media (max-width: 768px) {
   .wraper .box {
     margin: auto;
+  }
+  .main {
+    display: block;
+    text-align: center;
+    width: 90%;
+    border-bottom: 2px solid rgba(255, 255, 255, 0.75);
+  }
+  .main h3 {
+    font-size: 56px;
+  }
+  .main button {
+    margin: 0px auto;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    line-height: 56px;
+    font-size: 24px;
+    font-weight: 700;
+    padding: 0px 60px;
+    background-color: #211b1b00;
+    border: 3px solid var(--white);
+    color: var(--white);
+    cursor: pointer;
+    white-space: nowrap;
   }
 }
 </style>
