bf42a35972e9751458334bdb587f76446264a6f6
binghan60 <binghan60@gmail.com>
2024-10-28 12:18:50 +0800

    調整架構

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
+import NavNarComponent from '@/components/NavNarComponent.vue'
+
 import { RouterView } from 'vue-router'
 export default {
-  components: { RouterView },
+  components: { RouterView, NavNarComponent },
 }
 </script>
 
 <template>
+  <NavNarComponent></NavNarComponent>
   <RouterView />
 </template>
 
