6d7b3ce41f86ca4912d1513c0ad5b41282441357
binghan60 <binghan60@gmail.com>
2024-10-28 15:14:10 +0800

    新增路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import ProductListView from '../views/ProductListView.vue'
+import ContactusView from '../views/ContactusView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/productlist',
+      name: 'productlist',
+      component: ProductListView,
+    },
+    {
+      path: '/contactus',
+      name: 'contactus',
+      component: ContactusView,
     },
   ],
 })
---
+++ src/views/ContactusView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>contactus</div>
+</template>
