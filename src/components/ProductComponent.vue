7e7aa3a1b43c6eb4a21fe4221dcd86df773c8e0a
binghan60 <binghan60@gmail.com>
2024-10-29 10:12:27 +0800

    新增產品卡片元件

---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="product-card">
+    <img src="/img/dates.png" class="product-image" alt="商品照片" />
+    <div class="product-info">
+      <h5 class="product-title">13號 珍蜜</h5>
+      <div class="quantity-control">
+        <button class="quantity-button">-</button>
+        <input type="number" min="1" class="quantity-input" />
+        <button class="quantity-button">+</button>
+      </div>
+      <button class="add-to-cart">加入購物車</button>
+    </div>
+  </div>
+</template>
