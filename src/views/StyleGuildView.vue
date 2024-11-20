b6694988d141995e7f70a7dc2ec72ce74016da6a
binghan60 <binghan60@gmail.com>
2024-11-20 14:12:17 +0800

    新增標題

--- src/views/StyleGuildView.vue
+++ src/views/StyleGuildView.vue
@@ -7,23 +7,23 @@
       </div>
       <div class="col-span-12 text-center">Colors</div>
       <div class="col-span-4 lg:col-span-2 m-auto">
-        <div class="bg-primary w-10 lg:w-20 h-10 lg:h-20 rounded-full m-auto"></div>
+        <div class="bg-primary w-16 lg:w-20 h-16 lg:h-20 rounded-full m-auto"></div>
         <div class="text-center m-auto">#50653e</div>
       </div>
       <div class="col-span-4 lg:col-span-2 m-auto">
-        <div class="bg-secondary w-10 lg:w-20 h-10 lg:h-20 rounded-full m-auto"></div>
+        <div class="bg-secondary w-16 lg:w-20 h-16 lg:h-20 rounded-full m-auto"></div>
         <div class="text-center m-auto">#a8d08d</div>
       </div>
       <div class="col-span-4 lg:col-span-2 m-auto">
-        <div class="bg-accent1 w-10 lg:w-20 h-10 lg:h-20 rounded-full m-auto"></div>
+        <div class="bg-accent1 w-16 lg:w-20 h-16 lg:h-20 rounded-full m-auto"></div>
         <div class="text-center m-auto">#d1e058</div>
       </div>
       <div class="col-span-4 lg:col-span-2 m-auto">
-        <div class="bg-accent2 w-10 lg:w-20 h-10 lg:h-20 rounded-full m-auto"></div>
+        <div class="bg-accent2 w-16 lg:w-20 h-16 lg:h-20 rounded-full m-auto"></div>
         <div class="text-center m-auto">#f1a33e</div>
       </div>
       <div class="col-span-4 lg:col-span-2 m-auto">
-        <div class="bg-border w-10 lg:w-20 h-10 lg:h-20 rounded-full m-auto"></div>
+        <div class="bg-border w-16 lg:w-20 h-16 lg:h-20 rounded-full m-auto"></div>
         <div class="text-center m-auto">#d3d3d3</div>
       </div>
       <div class="col-span-4 lg:col-span-2 m-auto">
@@ -46,8 +46,24 @@
       <div class="col-span-6 text-center text-darkText">深色文字</div>
       <div class="col-span-6 text-center text-lightText">淺色文字</div>
       <div class="col-span-12 text-center">標題</div>
-      <div class="col-span-12 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">標題1</div>
-      <div class="col-span-12 text-center">標題2</div>
+      <div class="col-span-12">
+        <h1 class="text-4xl">這是一段標題</h1>
+      </div>
+      <div class="col-span-12">
+        <h2 class="text-3xl">這是一段標題</h2>
+      </div>
+      <div class="col-span-12">
+        <h3 class="text-2xl">這是一段標題</h3>
+      </div>
+      <div class="col-span-12">
+        <h4 class="text-xl">這是一段標題</h4>
+      </div>
+      <div class="col-span-12">
+        <h5 class="text-lg">這是一段標題</h5>
+      </div>
+      <div class="col-span-12">
+        <h6 class="text-base">這是一段標題</h6>
+      </div>
     </div>
   </div>
 </template>
